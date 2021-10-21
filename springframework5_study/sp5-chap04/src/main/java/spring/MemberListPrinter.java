e659362a766bfc3a0f67d9c0ccf17252d084d628
은지은 <eung0804@naver.com>
2021-10-21 18:03:23 +0900

    ReCommit

--- springframework5_study/sp5-chap04/src/main/java/spring/MemberListPrinter.java
+++ springframework5_study/sp5-chap04/src/main/java/spring/MemberListPrinter.java
@@ -1,11 +1,15 @@
 package spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.Collection;
 
 public class MemberListPrinter {
 
     private MemberDao memberDao;
     private MemberPrinter printer;
+
+    public MemberListPrinter() {}
 
     public MemberListPrinter(MemberDao memberDao, MemberPrinter printer) {
         this.memberDao = memberDao;
@@ -15,5 +19,15 @@ public class MemberListPrinter {
     public void printAll() {
         Collection<Member> members = memberDao.selectAll();
         members.forEach(m -> printer.print(m));
+    }
+
+    @Autowired
+    public void setMemberDao(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @Autowired
+    public void setPrinter(MemberPrinter printer) {
+        this.printer = printer;
     }
 }
