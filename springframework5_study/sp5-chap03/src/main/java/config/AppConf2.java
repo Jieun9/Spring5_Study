077442a1b8c9934bf296948ad53be0067dd3848f
은지은 <eung0804@naver.com>
2021-10-21 17:28:42 +0900

    @Import문 연습

--- springframework5_study/sp5-chap03/src/main/java/config/AppConf2.java
+++ springframework5_study/sp5-chap03/src/main/java/config/AppConf2.java
@@ -1,44 +1,41 @@
 package config;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.*;
 
 @Configuration
-public class AppCtx {
+public class AppConf2 {
 
-    @Bean
-    public MemberDao memberDao() {
-        return new MemberDao();
-    }
+    @Autowired
+    private MemberDao memberDao;
+    @Autowired
+    private MemberPrinter memberPrinter;
 
     @Bean
     public MemberRegisterService memberRegSvc() {
-        return new MemberRegisterService(memberDao());
+        return new MemberRegisterService(memberDao);
     }
 
     @Bean
     public ChangePasswordService changePwdSvc() {
         ChangePasswordService pwdSvc = new ChangePasswordService();
-        pwdSvc.setMemberDao(memberDao());
+        pwdSvc.setMemberDao(memberDao);
         return pwdSvc;
     }
 
     @Bean
-    public MemberPrinter memberPrinter() {
-        return new MemberPrinter();
-    }
-
-    @Bean
     public MemberListPrinter listPrinter() {
-        return new MemberListPrinter(memberDao(), memberPrinter());
+        return new MemberListPrinter(memberDao, memberPrinter);
     }
 
     @Bean
     public MemberInfoPrinter infoPrinter() {
         MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
-        infoPrinter.setMemberDao(memberDao());
-        infoPrinter.setPrinter(memberPrinter());
+        infoPrinter.setMemberDao(memberDao);
+        infoPrinter.setPrinter(memberPrinter);
         return infoPrinter;
     }
 
@@ -49,6 +46,5 @@ public class AppCtx {
         versionPrinter.setMinorVersion(0);
         return versionPrinter;
     }
-
 
 }
