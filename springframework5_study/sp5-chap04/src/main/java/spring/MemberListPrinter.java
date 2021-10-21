24b339ea73d4e072232e2307a4ca29ea83d0405f
은지은 <eung0804@naver.com>
2021-10-21 18:34:57 +0900

    의존 자동주입 연습

--- springframework5_study/sp5-chap04/src/main/java/spring/MemberListPrinter.java
+++ springframework5_study/sp5-chap04/src/main/java/spring/MemberListPrinter.java
@@ -1,6 +1,7 @@
 package spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.Collection;
 
@@ -27,6 +28,7 @@ public class MemberListPrinter {
     }
 
     @Autowired
+    @Qualifier("summaryPrinter")
     public void setPrinter(MemberPrinter printer) {
         this.printer = printer;
     }
